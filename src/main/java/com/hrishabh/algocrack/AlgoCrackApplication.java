3933b62301c22118b64c2568447ad0bbd15ee699
Hrishabh Joshi <hrishabhjoshi123@gmail.com>
2025-07-25 18:17:04 +0530

    Fixed some errors | Basic JWT based auth working

--- src/main/java/com/hrishabh/algocrack/AlgoCrackApplication.java
+++ src/main/java/com/hrishabh/algocrack/AlgoCrackApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("com.example.algocrackentityservice.models")
+@EnableJpaRepositories("com.hrishabh.algocrack.repository")
+
+@EntityScan("com.hrishabh.algocrackentityservice.models")
 @EnableJpaAuditing
 public class AlgoCrackApplication {
 
