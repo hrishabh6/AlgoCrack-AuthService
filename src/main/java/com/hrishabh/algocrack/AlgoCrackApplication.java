4f3817f3b2f59eaef8c378e188bfc97a33aa5b56
Hrishabh Joshi <hrishabhjoshi123@gmail.com>
2025-07-25 11:30:22 +0530

    Project created

---
+++ src/main/java/com/hrishabh/algocrack/AlgoCrackApplication.java
@@ -0,0 +1,13 @@
+package com.hrishabh.algocrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoCrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoCrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hrishabh/algocrack/AlgoCrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrishabh.algocrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoCrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
